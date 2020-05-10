cc265ec31f389ead5ff8e5af5f784cbf76da12ee
Emma Paris <shirl.shen@gmail.com>
2020-05-10 15:19:14 -0400

    Part1 exercises

---
+++ part1/anecdotes/src/index.js
@@ -0,0 +1,66 @@
+import React, {useState} from 'react'
+import ReactDOM from 'react-dom'
+
+// Button component
+const Header = ({text}) => <h1>{text}</h1>
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
+
+  //Create array of votes
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
+
+  // Event handler for next anecdote button
+  const pickRandom = () => {
+    let randomNumber = Math.floor(Math.random() * (anecdotes.length - 0) + 0)
+    return (setSelected(randomNumber))
+  }
+
+  // Event handler for vote button
+  const pickVote = () => {
+    let copy = [...votes]
+    copy[selected] +=1
+    return(setVotes(copy))
+  }
+
+  // Find index of anecdote with max votes
+  const maxVoteIndex = () =>{
+    let maxVotes = Math.max.apply(null, votes)
+    let index = votes.indexOf(maxVotes)
+    return(index)
+  }
+
+
+  
+  return (
+    <div>
+      <Header text="Anecdote of the day"/>
+      {props.anecdotes[selected]}
+      <br/>
+      has {votes[selected]} votes.
+      <br/>
+      <Button onClick={pickVote} text="vote"/>
+      <Button onClick={pickRandom} text="next anecdote"/>
+      <br/>
+      <Header text="Anecdote with most votes"/>
+      {props.anecdotes[maxVoteIndex()]}
+      <br/>
+      has {votes[maxVoteIndex()]} votes.
+    </div>
+  )
+}
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
+  document.getElementById("root")
+)
