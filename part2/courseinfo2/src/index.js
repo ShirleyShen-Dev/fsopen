466f3563872bd98a403671eeaf862c4de68ee91b
Emma Paris <shirl.shen@gmail.com>
2020-05-16 23:28:14 -0400

    courseinfo2

--- part2/courseinfo2/src/index.js
+++ part2/courseinfo2/src/index.js
@@ -1,65 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const Header = ({ course }) => {
-  return (
-    <h2>{course.name}</h2>
-  )
-}
-
-const Part = ({parts}) => {
-  // console.log(parts)
-  return (
-    <div>
-      {parts.map(part => 
-      <p key={part.id}>
-        {part.name} {part.exercises}
-        </p>
-      )}
-    </div>
-    )
-}
-
-const Content = ({ course }) => {
-  // console.log(course)
-  return (
-    <div>
-      <Part parts={course.parts}/>
-    </div>
-  )
-}
-
-const Total = ({ parts }) => {
-  // console.log(parts)
-  let array = []
-  parts.forEach((element) => {
-    array.push(element.exercises)
-  })
-  let sum = array.reduce((acc, curr) => acc + curr)
-  return(
-  <p>total of {sum} exercises</p>
-  ) 
-}
-
-
-const Course = ({courses}) => {
-  // console.log(courses)
-  return(
-      <div>
-      {courses.map(course => 
-        <div key={course.id}>
-        <Header course={course}/>
-        <Content course={course}/>
-        <strong>
-          <Total parts={course.parts} />
-        </strong>
-        </div>
-      )}
-    </div>
-
-  )
-}
-
+import Course from './components/course'
 
 const App = () => {
   const courses = [
