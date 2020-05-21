15d3cb3d037db5c24c54903966f944f329a34e52
Emma Paris <shirl.shen@gmail.com>
2020-05-21 10:13:58 -0400

    countries

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/filter'
 import PersonForm from './components/personForm'
 import Persons from './components/persons'
 
 const App = () => {
   // States
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [ persons, setPersons ] = useState([]) 
   const [ newName, setNewName ] = useState("")
   const [ newNumber, setNewNumber ] = useState("")
   const [ newFilter, setNewFilter ] = useState("")
@@ -33,6 +29,18 @@ const App = () => {
       setNewNumber("")
     }
   }
+  // Effect to fetch data from JSON server
+  useEffect(() => {
+    console.log("effect")
+    axios
+      .get("http://localhost:3001/persons")
+      .then(response => {
+        console.log("promise fulfilled")
+        setPersons(response.data)
+      })
+  }, [])
+  console.log("render", persons.length, "persons")
+
 
   // Event handlers
   const nameHandler = (event) => setNewName(event.target.value)
